459ae65f49f74de69750ff2dec1d35543f86acab
Paul Cross <me@paulcross.cz>
2025-12-23 01:18:50 +0100

    update theme

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,59 +1,57 @@
 return {
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     config = function()
-      vim.cmd.colorscheme("rose-pine")
-      -- Set all backgrounds to none for transparency
-      local groups = {
-        -- Core UI
-        "Normal", "NormalNC", "NormalFloat", "FloatBorder", "MsgArea", "MsgSeparator",
-        "StatusLine", "StatusLineNC", "VertSplit", "SignColumn", "CursorLine", "CursorLineNr", "LineNr", "Folded", "FoldColumn", "WinSeparator",
-        -- Telescope
-        "TelescopeNormal", "TelescopeBorder", "TelescopePromptNormal", "TelescopePromptBorder", "TelescopeResultsNormal", "TelescopeResultsBorder", "TelescopePreviewNormal", "TelescopePreviewBorder",
-        -- Completion
-        "Pmenu", "PmenuSel", "PmenuSbar", "PmenuThumb",
-        -- WhichKey
-        "WhichKeyFloat",
-        -- NvimTree
-        "NvimTreeNormal", "NvimTreeNormalNC", "NvimTreeEndOfBuffer",
-        -- cmp.nvim
-        "CmpPmenu", "CmpPmenuBorder", "CmpPmenuSel", "CmpPmenuSbar", "CmpPmenuThumb",
-        -- dressing.nvim
-        "DressingInputNormal", "DressingInputBorder",
-        -- fzf-lua
-        "FzfLuaNormal", "FzfLuaBorder",
-        -- LazyVim extras
-        "LazyNormal", "LazyBorder"
-      }
-      local function clear_bg()
-        for _, group in ipairs(groups) do
-          vim.api.nvim_set_hl(0, group, { bg = "none" })
-        end
-      end
-      clear_bg()
-      vim.api.nvim_create_autocmd("VimEnter", {
-        callback = clear_bg,
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          treesitter = true,
+          notify = true,
+          mini = true,
+        },
+        custom_highlights = function(colors)
+          return {
+            Normal = { bg = "NONE" },
+            NormalNC = { bg = "NONE" },
+            NormalFloat = { bg = "NONE" },
+            FloatBorder = { bg = "NONE" },
+            VertSplit = { bg = "NONE" },
+            SignColumn = { bg = "NONE" },
+            StatusLine = { bg = "NONE" },
+            StatusLineNC = { bg = "NONE" },
+            TabLine = { bg = "NONE" },
+            TabLineFill = { bg = "NONE" },
+            TabLineSel = { bg = "NONE" },
+            Pmenu = { bg = "NONE" },
+            PmenuSel = { bg = "NONE" },
+            PmenuSbar = { bg = "NONE" },
+            PmenuThumb = { bg = "NONE" },
+            WinSeparator = { bg = "NONE" },
+            CursorLine = { bg = "NONE" },
+            CursorLineNr = { bg = "NONE" },
+            LineNr = { bg = "NONE" },
+            Folded = { bg = "NONE" },
+            FoldColumn = { bg = "NONE" },
+            EndOfBuffer = { bg = "NONE" },
+          }
+        end,
       })
-
-      -- Fix invisible popup selection and border backgrounds
-      vim.api.nvim_set_hl(0, "PmenuSel", { fg = "#000000", bg = "#ffffff", bold = true })
-      vim.api.nvim_set_hl(0, "Pmenu", { fg = "#ffffff", bg = "#222222" })
-      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#888888", bg = "NONE" })
-      vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#888888", bg = "NONE" })
-      vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#888888", bg = "NONE" })
-      vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#888888", bg = "NONE" })
-      vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = "#888888", bg = "NONE" })
-      vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "NONE" })
-      vim.api.nvim_set_hl(0, "WhichKeyBorder", { fg = "#888888", bg = "NONE" })
-    end
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine"
-    }
-  }
+      colorscheme = "catppuccin",
+      transparent = true,
+    },
+  },
 }
-
