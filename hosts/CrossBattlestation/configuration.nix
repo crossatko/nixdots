7448b1fca6de48e6414f38ccc22f69a73e4708d1
Paul Cross <me@paulcross.cz>
2025-12-25 22:26:40 +0100

    enable hibernation

--- hosts/CrossBattlestation/configuration.nix
+++ hosts/CrossBattlestation/configuration.nix
@@ -8,21 +8,27 @@
     ../shared/system.nix
     ../shared/user.nix
     ../shared/programs.nix
-
   ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.resumeDevice = "/dev/mapper/luks-e9e4b34e-873b-41da-85c5-da16ad7c17c1";
+
+  boot.kernelParams = [
+    "resume=/dev/mapper/luks-e9e4b34e-873b-41da-85c5-da16ad7c17c1"
+    "pcie_aspm=off"
+  ];
+
+  boot.initrd.luks.devices."luks-e9e4b34e-873b-41da-85c5-da16ad7c17c1".device = "/dev/disk/by-uuid/e9e4b34e-873b-41da-85c5-da16ad7c17c1";
+  powerManagement.enable = true;
+
   boot.kernelPackages = pkgs.linuxPackages_latest;
   hardware.enableAllFirmware = true;
 
-  boot.initrd.luks.devices."luks-e9e4b34e-873b-41da-85c5-da16ad7c17c1".device = "/dev/disk/by-uuid/e9e4b34e-873b-41da-85c5-da16ad7c17c1";
-
   networking.hostName = "CrossBattlestation";
   networking.networkmanager.enable = true;
-
 
   hardware.bluetooth = {
     enable = true;
