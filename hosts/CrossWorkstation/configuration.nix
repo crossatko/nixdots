06fce0f14b9e959f1f2e1bfdf58e49aa5f33a942
Paul Cross <me@paulcross.cz>
2026-01-06 16:17:47 +0100

    ...

--- hosts/CrossWorkstation/configuration.nix
+++ hosts/CrossWorkstation/configuration.nix
@@ -1,21 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      ../shared/services.nix
-      ../shared/language-time.nix
-      ../shared/system.nix
-      ../shared/user.nix
-      ../shared/programs.nix
-    ];
-
+  imports = [
+    ./hardware-configuration.nix
+    ../shared/services.nix
+    ../shared/language-time.nix
+    ../shared/system.nix
+    ../shared/user.nix
+    ../shared/programs.nix
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.initrd.luks.devices."luks-e2d7e961-ee37-4c99-8495-da1295db1ee1".device = "/dev/disk/by-uuid/e2d7e961-ee37-4c99-8495-da1295db1ee1";
+  boot.kernelModules = [ "snd-aloop" ];
+
+  boot.initrd.luks.devices."luks-e2d7e961-ee37-4c99-8495-da1295db1ee1".device =
+    "/dev/disk/by-uuid/e2d7e961-ee37-4c99-8495-da1295db1ee1";
   networking.hostName = "CrossWorkstation"; # Define your hostname.
 
   networking.networkmanager.enable = true;
@@ -23,6 +24,11 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true; # Helps with battery info and some headset features
+      };
+    };
   };
   hardware.i2c.enable = true;
 
