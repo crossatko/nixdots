745553a3611ba1e9ff2d9dc1ac6513799f782a2c
Paul Cross <me@paulcross.cz>
2026-01-12 17:09:50 +0100

    update

--- hosts/CrossWorkstation/configuration.nix
+++ hosts/CrossWorkstation/configuration.nix
@@ -9,43 +9,56 @@
     ../shared/user.nix
     ../shared/programs.nix
   ];
+  boot = {
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelModules = [ "snd-aloop" ];
+    kernelModules = [ "snd-aloop" ];
 
-  boot.initrd.luks.devices."luks-e2d7e961-ee37-4c99-8495-da1295db1ee1".device =
-    "/dev/disk/by-uuid/e2d7e961-ee37-4c99-8495-da1295db1ee1";
-  networking.hostName = "CrossWorkstation"; # Define your hostname.
+    initrd = {
+      kernelModules = [ "amdgpu" ];
 
-  networking.networkmanager.enable = true;
+      luks.devices."luks-e2d7e961-ee37-4c99-8495-da1295db1ee1".device =
+        "/dev/disk/by-uuid/e2d7e961-ee37-4c99-8495-da1295db1ee1";
+    };
 
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        Experimental = true; # Helps with battery info and some headset features
+  };
+  networking = {
+
+    hostName = "CrossWorkstation"; # Define your hostname.
+
+    networkmanager.enable = true;
+    firewall.allowedTCPPorts = [ 53317 ];
+    firewall.allowedUDPPorts = [ 53317 ];
+  };
+
+  hardware = {
+
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Experimental = true; # Helps with battery info and some headset features
+        };
       };
     };
-  };
-  hardware.i2c.enable = true;
+    i2c.enable = true;
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    package = pkgs.mesa;
-    extraPackages = with pkgs; [
-      libva
-      libva-utils
-      rocmPackages.clr.icd
-      libva-vdpau-driver
-      libvdpau-va-gl
-    ];
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      package = pkgs.mesa;
+      extraPackages = with pkgs; [
+        libva
+        libva-utils
+        rocmPackages.clr.icd
+        libva-vdpau-driver
+        libvdpau-va-gl
+      ];
+    };
   };
-  networking.firewall.allowedTCPPorts = [ 53317 ];
-  networking.firewall.allowedUDPPorts = [ 53317 ];
 
   environment.systemPackages = with pkgs; [
     vulkan-tools
