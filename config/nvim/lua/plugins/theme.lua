f329e53a2d42dd03dfa01203d4b754ce180e2185
Paul Cross <me@paulcross.cz>
2026-01-15 12:56:15 +0100

    highlight active nvim tab

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -17,6 +17,9 @@ return {
           mini = true,
         },
         custom_highlights = function(colors)
+          local active_bg = colors.surface2
+          local active_fg = colors.mauve
+
           return {
             Normal = { bg = "NONE" },
             NormalNC = { bg = "NONE" },
@@ -26,9 +29,6 @@ return {
             SignColumn = { bg = "NONE" },
             StatusLine = { bg = "NONE" },
             StatusLineNC = { bg = "NONE" },
-            TabLine = { bg = "NONE" },
-            TabLineFill = { bg = "NONE" },
-            TabLineSel = { bg = "NONE" },
             Pmenu = { bg = "NONE" },
             PmenuSel = { bg = "NONE" },
             PmenuSbar = { bg = "NONE" },
@@ -40,6 +40,20 @@ return {
             Folded = { bg = "NONE" },
             FoldColumn = { bg = "NONE" },
             EndOfBuffer = { bg = "NONE" },
+
+            BufferLineFill = { bg = "NONE" },
+            BufferLineBufferSelected = { bg = active_bg, fg = active_fg, bold = true },
+            BufferLineModifiedSelected = { bg = active_bg, fg = colors.peach },
+            BufferLineErrorSelected = { bg = active_bg, fg = colors.red, bold = true },
+            BufferLineWarningSelected = { bg = active_bg, fg = colors.yellow, bold = true },
+            BufferLineInfoSelected = { bg = active_bg, fg = colors.blue, bold = true },
+            BufferLineHintSelected = { bg = active_bg, fg = colors.teal, bold = true },
+            BufferLineErrorDiagnosticSelected = { bg = active_bg, fg = colors.red, bold = true },
+            BufferLineWarningDiagnosticSelected = { bg = active_bg, fg = colors.yellow, bold = true },
+            BufferLineInfoDiagnosticSelected = { bg = active_bg, fg = colors.blue, bold = true },
+            BufferLineHintDiagnosticSelected = { bg = active_bg, fg = colors.teal, bold = true },
+            BufferLineSeparatorSelected = { bg = active_bg, fg = active_bg },
+            BufferLineCloseButtonSelected = { bg = active_bg, fg = colors.overlay0 },
           }
         end,
       })
